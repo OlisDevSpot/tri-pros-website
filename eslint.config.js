1b79095855ebc1cc194414052409c917b77e12d2
Oliver P <solutionsbyoli@gmail.com>
2025-12-07 10:09:57 -0800

    add ignores for payload types file

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   react: true,
+  ignores: ['src/payload-types.ts'],
   rules: {
     'react-refresh/only-export-components': 'off',
     'ts/no-empty-object-type': 'off',
