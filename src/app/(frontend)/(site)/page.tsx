52b2acec9efb1c0ba46f828c847c2ca8ba821132
Oliver P <solutionsbyoli@gmail.com>
2025-12-06 23:51:35 -0800

    update home metadata

--- src/app/(frontend)/(site)/page.tsx
+++ src/app/(frontend)/(site)/page.tsx
@@ -1,9 +1,14 @@
+import type { Metadata } from 'next'
 import BottomCTA from '@/components/cta'
 import HomeHero from '@/features/landing/ui/components/home/home-hero'
 import PastProjects from '@/features/landing/ui/components/home/past-projects'
 import ServicesPreview from '@/features/landing/ui/components/home/services-preview'
 import TestimonialsSection from '@/features/landing/ui/components/home/testimonials'
 import ValuePropositions from '@/features/landing/ui/components/home/value-proposition'
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
 
 export default function Home() {
   return (
