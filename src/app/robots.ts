c4bf58068abc55ad64e256cdf4baf62c6080aefc
Oliver P <solutionsbyoli@gmail.com>
2025-12-07 00:32:39 -0800

    fix robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-export function robots(): MetadataRoute.Robots {
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
