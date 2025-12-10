f3381805028d1d540ab44d8e82496a911ecf52b6
Oliver P <solutionsbyoli@gmail.com>
2025-12-09 21:00:28 -0800

    add a payload file to eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   react: true,
-  ignores: ['src/payload-types.ts'],
+  ignores: ['src/payload-types.ts', 'src/app/(payload)/admin/importMap.js'],
   rules: {
     'react-refresh/only-export-components': 'off',
     'ts/no-empty-object-type': 'off',
