0c3d502ca2cc61f51db7044e3cdc12ecc64b2523
Oliver P <solutionsbyoli@gmail.com>
2025-12-27 23:07:42 -0800

    move Proposal component from proposal page

--- src/app/(frontend)/proposal/page.tsx
+++ src/app/(frontend)/proposal/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { proposalSteps } from '@/features/proposals/constants/proposal-steps'
+import { Proposal } from '@/features/proposals/ui/components/proposal'
 import { ProposalPageNavbar } from '@/features/proposals/ui/components/proposal-page-navbar'
 
 export default function ProposalPage() {
@@ -14,23 +14,6 @@ export default function ProposalPage() {
       <ProposalPageNavbar />
       <div className="grow min-h-0 p-8">
         <Proposal />
-      </div>
-    </div>
-  )
-}
-
-export function Proposal() {
-  return (
-    <div className="h-full overflow-auto scroll-smooth">
-      <div className="container p-0 lg:p-0 pr-8 py-10 space-y-20">
-        {proposalSteps.map(step => (
-          <div
-            id={step.accessor}
-            key={step.accessor}
-          >
-            {step.Component}
-          </div>
-        ))}
       </div>
     </div>
   )
--- src/features/proposals/ui/components/proposal/index.tsx
+++ src/features/proposals/ui/components/proposal/index.tsx
@@ -1,4 +1,23 @@
+import { proposalSteps } from '@/features/proposals/constants/proposal-steps'
+
 export { BasicInfo } from './basic-info'
 export { Heading } from './heading'
 export { ProjectOverview } from './project-overview'
 export { RelatedProjects } from './related-projects'
+
+export function Proposal() {
+  return (
+    <div className="h-full overflow-auto scroll-smooth">
+      <div className="container p-0 lg:p-0 pr-8 py-10 space-y-20">
+        {proposalSteps.map(step => (
+          <div
+            id={step.accessor}
+            key={step.accessor}
+          >
+            {step.Component}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
