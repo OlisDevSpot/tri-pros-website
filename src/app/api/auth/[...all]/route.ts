8f81f16777892d1b6ec65cd7b997d48799046bf7
Oliver P <solutionsbyoli@gmail.com>
2026-01-06 20:02:50 -0800

    add node runtime to auth endpoints

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,6 @@
 import { toNextJsHandler } from 'better-auth/next-js'
 import { auth } from '@/shared/auth/server'
 
+export const runtime = 'nodejs'
+
 export const { GET, POST } = toNextJsHandler(auth)
