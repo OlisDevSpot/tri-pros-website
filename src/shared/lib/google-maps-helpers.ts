638efa141d1be5266d370eb5e55d97d461d16dcf
Oliver P <solutionsbyoli@gmail.com>
2026-03-22 18:59:55 -0700

    feat: add parseAddressComponents to google-maps-helpers

--- src/shared/lib/google-maps-helpers.ts
+++ src/shared/lib/google-maps-helpers.ts
@@ -39,3 +39,39 @@ function getCityName(addressComponents: google.maps.GeocoderAddressComponent[]) 
 
   return locality ? locality.long_name : ''
 }
+
+export interface LatLng {
+  lat: number
+  lng: number
+}
+
+export interface AddressFields {
+  address: string
+  city: string
+  state: string
+  zip: string
+  fullAddress: string
+  location: LatLng | null
+}
+
+export function parseAddressComponents(place: google.maps.places.PlaceResult): AddressFields {
+  const components = place.address_components ?? []
+
+  const get = (type: string) =>
+    components.find(c => c.types.includes(type))?.long_name ?? ''
+  const getShort = (type: string) =>
+    components.find(c => c.types.includes(type))?.short_name ?? ''
+
+  const streetNumber = get('street_number')
+  const route = get('route')
+  const address = [streetNumber, route].filter(Boolean).join(' ')
+  const city = getCityName(components)
+  const state = getShort('administrative_area_level_1')
+  const zip = get('postal_code')
+  const fullAddress = place.formatted_address ?? [address, city, state].filter(Boolean).join(', ')
+  const location = place.geometry?.location
+    ? { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }
+    : null
+
+  return { address, city, state, zip, fullAddress, location }
+}
